00b3e897989efd8a19ab16b003d9910f8369ec15
alizadasaleh <helas2003@gmail.com>
2022-03-05 12:47:09 +0400

    particle Class

---
+++ pso.py
@@ -0,0 +1,35 @@
+import random 
+import numpy as np
+
+def equation(x,y):
+    return (x-3.14)**2 + (y-2.72)**2 + np.sin(3*x+1.41) + np.sin(4*y-1.73)
+
+global_best = None
+c = 2
+class Particle:
+    def __init__(self,position):
+        self.position = position
+        self.velocity = [random.random() for i in range(len(position))]
+        self.pbest = position
+    def setVelocity(self,vel):
+        self.velocity = vel
+        
+
+
+    
+if __name__ == "__main__":
+    n_particles = 5
+    particles = []
+    for i  in range(n_particles):
+        particles.append(Particle( [random.randint(0,15),random.randint(0,15)] ))
+        print(particles[i].velocity)
+    particles = np.array(particles)
+    for particle  in particles:
+        print(particle.position)
+        particle.position = np.sum([particle.position,particle.velocity],axis=0)
+        print(particle.position)
+
+
+    
+    
+#
