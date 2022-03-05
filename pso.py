4e257fedbc4e5cc07a8e44241ac6f4f862a15015
alizadasaleh <helas2003@gmail.com>
2022-03-05 20:25:50 +0400

    clobration

--- pso.py
+++ pso.py
@@ -11,8 +11,9 @@ from numpy import pi
 
 global_best = [0,0]
 global_best_z = 0
-c = 0.25
-W = 0.01
+c = 0.1
+W = 0.8
+
 
 
 
@@ -28,7 +29,7 @@ def equation(x, y):
 class Particle:
     def __init__(self, position):
         self.position = position
-        self.velocity = [random.uniform(-1,1) for i in range(len(position))]
+        self.velocity = np.array([random.uniform(-1,1) for i in range(len(position))])
         global global_best_z
         global global_best
         x,y = position
@@ -45,7 +46,7 @@ class Particle:
         x, y = self.position
         return equation(x, y)
     def pb_pc_difference(self):
-        return np.sum([self.pbest, - self.position], axis=0)
+        return np.sum([self.pbest, -self.position], axis=0)
     def gb_pc_difference(self):
         return np.sum([global_best,-self.position], axis=0)
 
@@ -54,7 +55,6 @@ class Particle:
     
     def updateVelocity(self):
         self.velocity = W*self.velocity + c * random.uniform(-1,1) * self.pb_pc_difference() + c * random.uniform(-1,1) * self.gb_pc_difference()
-    
     def move(self):
 
         self.position = np.sum([self.position,self.velocity],axis=0)
@@ -67,29 +67,26 @@ class Particle:
         x,y = self.pbest
         if  equation(x,y) > self.z_pos():
             self.pbest = self.position
+        self.updateVelocity()
 
 def show_points(particles):
     scat = []
     for i in particles:
         scat.append(ax.scatter(i.position[0], i.position[1], i.z_pos(), s=30, c='black'))
-    plt.pause(0.1)
+    plt.pause(0.01)
 
 
-if __name__ == "__main__":
-    n_particles = 5
+if __name__ == "__main__": 
+    n_particles = 30
     particles = []
     for i in range(n_particles):
         particles.append(Particle( [random.uniform(-32.768,32.768),random.uniform(-32.768,32.768)] ))
 
-
-
-
-
     for x in range(30):
-        show_points(particles)
 
         for particle in particles:
             particle.move()
+    show_points(particles)
 
 
     x_data = np.arange(-32,32,0.5)
