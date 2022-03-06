f4d210bb7d20389bc2e12ffc101e45e121941bf3
alizadasaleh <helas2003@gmail.com>
2022-03-06 06:45:05 +0400

    n_dimension_done

--- pso.py
+++ pso.py
@@ -12,7 +12,7 @@ global W
 global equation
 ax = plt.axes(projection='3d')
 
-def schwefel(*args):
+def schwefel(args):
     answer = 418.9829*len(args)
     for arg in args:
 
@@ -20,7 +20,7 @@ def schwefel(*args):
     return answer
     # - x * sin( sqrt( abs( x )))-y*sin(sqrt(abs(y)))
 
-def ackley(*args):
+def ackley(args):
     first_part, second_part = 0, 0
     for arg in args:
         first_part += arg**2
@@ -28,14 +28,14 @@ def ackley(*args):
     answer = -20*exp(-0.2*sqrt(1/len(args)*first_part))-exp(1/len(args)*second_part)+20+exp(1)
     return answer
 
-def rosenbrock(*args):
+def rosenbrock(args):
     answer = 0
     for i in range(len(args)-1):
         answer += 100* (args[i+1]- args[i]**2 )**2 +(args[i]-1)**2
     return answer
 
 
-def rastrigin(*args):
+def rastrigin(args):
     answer = 10*len(args)
     for arg in args:
         answer += arg**2 - 10* cos(2*pi*arg)
@@ -47,9 +47,7 @@ class Particle:
         self.velocity = np.array([random.uniform(-1,1) for i in range(len(position))])
         global global_best_z
         global global_best
-        x,y = position
-
-        self.current_z = equation(x,y)
+        self.current_z = equation(position)
         self.pbest = position
         self.pbest_z = self.current_z
         if global_best_z > self.current_z:
@@ -60,8 +58,7 @@ class Particle:
         self.velocity = vel
 
     def z_pos(self):
-        x, y = self.position
-        return equation(x, y)
+        return equation(self.position)
     def pb_pc_difference(self):
         return np.sum([self.pbest, -self.position], axis=0)
     def gb_pc_difference(self):
@@ -75,10 +72,9 @@ class Particle:
     def move(self):
 
         self.position = np.sum([self.position,self.velocity],axis=0)
-        x,y = self.position
         global global_best_z
         global global_best
-        current_z = equation(x,y)
+        current_z = equation(self.position)
         if global_best_z > current_z:
             global_best_z = current_z
             global_best = self.position
@@ -101,117 +97,135 @@ def show_points(particles):
         scat.append(ax.scatter(i.position[0], i.position[1], i.z_pos(), s=5, c='black'))
 
 
-def ackley_run():
+def ackley_run(dimension):
     global global_best
     global global_best_z
     global c
     global W
     global equation
+    global d
     equation = ackley
-    global_best = [0,0]
     global_best_z = 0
     c = 0.9
     W = 0.8
+    d = dimension
+    global_best = d*[0]
     n_particles = 30
     particles = []
     for i in range(n_particles):
-        particles.append(Particle( [random.uniform(-32.768,32.768),random.uniform(-32.768,32.768)] ))
+        particles.append(Particle( d*[random.uniform(-32.768,32.768)] ))
 
     for x in range(30):
         for particle in particles:
             particle.move()
     show_points(particles)
+    if d == 3:
+        x_data = np.arange(-32,32,0.5)
+        y_data = np.arange(-32,32,0.5)
+        X, Y = np.meshgrid(x_data, y_data)
+        Z = equation([X,Y])
+        ax.plot_surface(X, Y, Z, alpha=0.5)
+        plt.show()
+    if d > 3:
+        print([particle.current_z for particle in particles])
 
-    x_data = np.arange(-32,32,0.5)
-    y_data = np.arange(-32,32,0.5)
-    X, Y = np.meshgrid(x_data, y_data)
-    Z = equation(X,Y)
-    ax.plot_surface(X, Y, Z, alpha=0.5)
-    plt.show()
-
-def rosenbrock_run():
+def rosenbrock_run(dimension):
     global equation
     global global_best
     global global_best_z
     global c
     global W
+    global d
+    d = dimension
     c = 0.1
     W = 0.8
-    equation = rosenbrock
-    global_best = [1,1]
-    global_best_z = 0
-    n_particles = 30
-    particles = []
-    for i in range(n_particles):
-        particles.append(Particle( [random.uniform(-5,10),random.uniform(-5,10)] ))
-
-    for x in range(30):
-        for particle in particles:
-            particle.move()
-    show_points(particles)
-
-    x_data = np.arange(-5,10,0.2)
-    y_data = np.arange(-5,10,0.2)
-    X, Y = np.meshgrid(x_data, y_data)
-    Z = equation(X,Y)
-    ax.plot_surface(X, Y, Z, alpha=0.5)
-    plt.show()
-
-def schewefel_run():
-    global equation
-    global global_best
-    global global_best_z
-    global c
-    global W
-    c = 0.1
-    W = 0.8
-    equation = schwefel
-    global_best = [420.9687,420.9687]
-    global_best_z = 0
-    n_particles = 30
-    particles = []
-    for i in range(n_particles):
-        particles.append(Particle( [random.uniform(-500,500),random.uniform(-500,500)]))
-
-    for x in range(30):
-        for particle in particles:
-            particle.move()
-    show_points(particles)
-
-    x_data = np.arange(-500,500,20)
-    y_data = np.arange(-500,500,20)
-    X, Y = np.meshgrid(x_data, y_data)
-    Z = equation(X,Y)
-    ax.plot_surface(X, Y, Z, alpha=0.5)
-    plt.show()
     
-def rastrigin_run():
+    equation = rosenbrock
+    global_best = d*[1]
+    global_best_z = 0
+    n_particles = 30
+    particles = []
+    for i in range(n_particles):
+        particles.append(Particle( d*[random.uniform(-5,10)] ))
+
+    for x in range(30):
+        for particle in particles:
+            particle.move()
+    if d == 3:
+        show_points(particles)
+
+        x_data = np.arange(-5,10,0.2)
+        y_data = np.arange(-5,10,0.2)
+        X, Y = np.meshgrid(x_data, y_data)
+        Z = equation([X,Y])
+        ax.plot_surface(X, Y, Z, alpha=0.5)
+        plt.show()
+    if d > 3:
+        print([particle.position for particle in particles])
+
+def schewefel_run(dimension):
     global equation
     global global_best
     global global_best_z
     global c
     global W
+    global d
     c = 0.1
     W = 0.8
-    equation = rastrigin
-    global_best = [0,0]
+    d = dimension
+    equation = schwefel
+    global_best = d * [420.9687]
     global_best_z = 0
     n_particles = 30
     particles = []
     for i in range(n_particles):
-        particles.append(Particle( [random.uniform(-5.12, 5.12),random.uniform(-5.12, 5.12)]))
+        particles.append(Particle( d*[random.uniform(-500,500)]))
 
     for x in range(30):
         for particle in particles:
             particle.move()
-    show_points(particles)
+    if d == 3:
+        show_points(particles)
+        x_data = np.arange(-500,500,20)
+        y_data = np.arange(-500,500,20)
+        X, Y = np.meshgrid(x_data, y_data)
+        Z = equation([X,Y])
+        ax.plot_surface(X, Y, Z, alpha=0.5)
+        plt.show()
+    if d > 3:
+        print([particle.current_z for particle in particles])
+    
+def rastrigin_run(dimension):
+    global equation
+    global global_best
+    global global_best_z
+    global c
+    global W
+    global d
+    c = 0.1
+    W = 0.8
+    d = dimension
+    equation = rastrigin
+    global_best = d*[0]
+    global_best_z = 0
+    n_particles = 30
+    particles = []
+    for i in range(n_particles):
+        particles.append(Particle( d*[random.uniform(-5.12, 5.12)] ))
 
-    x_data = np.arange(-5.12, 5.12,1)
-    y_data = np.arange(-5.12, 5.12,1)
-    X, Y = np.meshgrid(x_data, y_data)
-    Z = equation(X,Y)
-    ax.plot_surface(X, Y, Z, alpha=0.5)
-    plt.show()
+    for x in range(30):
+        for particle in particles:
+            particle.move()
+    if d == 3:
+        show_points(particles)
+        x_data = np.arange(-5.12, 5.12,0.1)
+        y_data = np.arange(-5.12, 5.12,0.1)
+        X, Y = np.meshgrid(x_data, y_data)
+        Z = equation([X,Y])
+        ax.plot_surface(X, Y, Z, alpha=0.5)
+        plt.show()
+    if d == 5:
+        print([particle.position for particle in particles])
 
 def mean(particles):
     answer = np.zeros(len(particles[0].position))
@@ -220,8 +234,5 @@ def mean(particles):
     return answer
 
 if __name__ == "__main__": 
-    rastrigin_run()
-
-
-
+    ackley_run(3)
 
